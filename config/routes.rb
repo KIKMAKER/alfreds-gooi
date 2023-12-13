de0adbb24482954cdcabd0f1b17bcfc7f5de0025
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-13 19:53:26 +0200

    seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+
+  get 'get_csv', to: 'collections#get_csv'
+  post 'import_csv', to: 'collections#import_csv'
+
+  resources :subscriptions do
+    resources :collections, only: %i[new create]
+    collection do
+      get :tuesday
+      get :wednesday
+    end
+  end
+
+  get 'drivers_logs/start', to: 'drivers_logs#start'
+  post 'drivers_logs/start', to: 'drivers_logs#start'
+  get 'drivers_logs/end', to: 'drivers_logs#end'
+  patch 'drivers_logs/end', to: 'drivers_logs#end'
+
+  resources :collections, only: %i[edit update]
 end
