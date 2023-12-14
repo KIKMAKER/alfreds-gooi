fb6d1812fe1b43ff5fe010bb5500de35ae1bb787
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-14 22:38:12 +0200

    still working on a few collections not being created by the import method and displaying skipping collections on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @subscriptions = Subscription.where(collection_day: Date.today.strftime("%A")).order(:collection_order)
+    # in production today will be the current day,
+    # today = Date.today.strftime("%A")
+    # but in testing I want to be able to test the view for a given day
+    today = "Wednesday"
+    @subscriptions = Subscription.where(collection_day: today).order(:collection_order)
     @skip_subscriptions = @subscriptions.select { |subscription| subscription.collections.last.skip == true }
   end
 end
