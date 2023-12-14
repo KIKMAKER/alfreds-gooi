dc9656d9323f08ac52ce7dac1aed1afcd42c1ee6
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-14 17:19:53 +0200

    updated edit form with new attributes and some styling

---
+++ app/controllers/drivers_days_controller.rb
@@ -0,0 +1,2 @@
+class DriversDaysController < ApplicationController
+end
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,5 @@
 class SubscriptionsController < ApplicationController
+  # pretty much standard CRUD stuff
   def index
     @subscriptions = Subscription.all
   end
@@ -12,8 +13,11 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new
   end
 
+  # a special view that will load all of the collections for a given day
   def today
+    # in production today will be the current day,
     # today = Date.today.strftime("%A")
+    # but in testing I want to be able to test the view for a given day
     today = "Wednesday"
     @subscriptions = Subscription.where(collection_day: today).order(:collection_order)
   end
