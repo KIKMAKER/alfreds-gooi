5ecbad8af86336991d8bd73e92a4bd664bd264f7
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-17 16:14:13 +0200

    USING on DD change migration but really this time

--- db/migrate/20231217071738_change_start_kms_and_end_kms_to_integer.rb
+++ db/migrate/20231217071738_change_start_kms_and_end_kms_to_integer.rb
@@ -1,13 +1,21 @@
 class ChangeStartKmsAndEndKmsToInteger < ActiveRecord::Migration[7.0]
   def up
-    # Convert start_kms and end_kms to integer, using explicit casting
-    change_column :drivers_days, :start_kms, 'integer USING CAST(start_kms AS integer)'
-    change_column :drivers_days, :end_kms, 'integer USING CAST(end_kms AS integer)'
+    # Remove the old columns
+    remove_column :drivers_days, :start_kms
+    remove_column :drivers_days, :end_kms
+
+    # Add new integer columns
+    add_column :drivers_days, :start_kms, :integer
+    add_column :drivers_days, :end_kms, :integer
   end
 
   def down
-    # Convert back to datetime if you roll back this migration
-    change_column :drivers_days, :start_kms, :datetime
-    change_column :drivers_days, :end_kms, :datetime
+    # Remove the integer columns
+    remove_column :drivers_days, :start_kms
+    remove_column :drivers_days, :end_kms
+
+    # Add back the datetime columns
+    add_column :drivers_days, :start_kms, :datetime
+    add_column :drivers_days, :end_kms, :datetime
   end
 end
