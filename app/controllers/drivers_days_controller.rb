f37d6d41e122e81c06233ee883dfcc26167cef76
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-18 12:16:02 +0200

    today set for production

--- app/controllers/drivers_days_controller.rb
+++ app/controllers/drivers_days_controller.rb
@@ -3,10 +3,14 @@ class DriversDaysController < ApplicationController
 
   def start
     # in production today will be the current day,
-    today = (Date.today + 2)
+    # today = "Wednesday"
+    # PRODUCTION
+    today = Date.today
     @today = today.strftime("%A")
     # but in testing I want to be able to test the view for a given day
-    # today = "Wednesday"
+    # DEVELOPMENT
+    # today = (Date.today + 2)
+    # @today = today.strftime("%A")
     @drivers_day = DriversDay.find_or_create_by(date: today)
     @subscriptions = Subscription.where(collection_day: @today).order(:collection_order)
     @skip_subscriptions = @subscriptions.select { |subscription| subscription.collections.last&.skip == true }
