1d8b7517b8c538821426933f3ce287a6920ba82b
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-19 07:25:45 +0200

    date is dynamic

--- app/controllers/drivers_days_controller.rb
+++ app/controllers/drivers_days_controller.rb
@@ -5,11 +5,10 @@ class DriversDaysController < ApplicationController
     # in production today will be the current day,
     # today = "Wednesday"
     # PRODUCTION
-    # today = Date.today
-    # @today = today.strftime("%A")
+    today = Date.today
     # but in testing I want to be able to test the view for a given day
     # DEVELOPMENT
-    today = (Date.today + 1)
+    # today = (Date.today + 1)
     @today = today.strftime("%A")
     # ##
     @drivers_day = DriversDay.find_or_create_by(date: today)
