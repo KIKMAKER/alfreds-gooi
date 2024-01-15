01a68f457ac54bad1b9425db9bb58071681100bb
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2024-01-15 23:54:25 +1100

    bucket is float, drop off button changes new_customer to false, can add kms on home page

--- db/migrate/20240114105617_change_data_type_from_string_to_float.rb
+++ db/migrate/20240114105617_change_data_type_from_string_to_float.rb
@@ -1,12 +1,10 @@
 class ChangeDataTypeFromStringToFloat < ActiveRecord::Migration[7.0]
-  def change
-    def up
-      change_column :collections, :buckets, :integer, using: 'buckets::float'
-    end
+  def up
+    change_column :collections, :buckets, :float, using: 'buckets::float'
+  end
 
-    def down
-      # Change the data types back to the original types if you need to roll back
-      change_column :collections, :buckets, :string
-    end
+  def down
+    # Change the data types back to the original types if you need to roll back
+    change_column :collections, :buckets, :string
   end
 end
