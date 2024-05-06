3f8afc90996acf4f710d9174b04160653c5d5f13
Chris Roberts <chrisrobertscollective@gmail.com>
2024-05-06 17:37:26 +0200

    video working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,7 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+
+# Chris asdding this to get video to work
+Rails.application.config.assets.precompile += %w( Our_Mission_at_Soil_For_Life.mp4 )
+
