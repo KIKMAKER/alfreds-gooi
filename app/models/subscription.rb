40ab787822758cb0feb2e4911334e3e513cb4ad2
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2024-05-16 17:45:29 +0200

    subscription summary methods and displaying on the view + sub collections index

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -16,6 +16,29 @@ class Subscription < ApplicationRecord
 
   # customised methods
 
+  def total_collections
+    collections.count
+  end
+
+  def skipped_collections
+    collections.where(skip: true).count
+  end
+
+  def total_bags
+    collections.sum(:bags)
+  end
+
+  def total_buckets
+    collections.sum(:buckets)
+  end
+
+  def total_bags_last_n_months(n)
+    collections.where("created_at >= ?", n.months.ago).sum(:bags)
+  end
+
+
+
+
   def self.active_subs_for(day)
     all.where(collection_day: day).includes(:collections).order(:collection_order)
   end
