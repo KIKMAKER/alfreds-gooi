aa6d39f0ff23b85fe258318f5f77ff6fabc30eac
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-06-26 21:08:40 +0200

    adding cusomer_id to user model so that it can be created afteruser is created

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,17 @@ class Invoice < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
   validates :issued_date, :due_date, :total_amount, presence: true
+  after_commit :set_number, on: :create
+  ## custom methods
+
+  def set_number
+    last_invoice = Invoice.last
+    self.number = if last_invoice.nil?
+                            1
+                          elsif Invoice.last.number.to_i
+                            Invoice.last.number.to_i + 1
+                          else
+                            Invoice.last.number
+                          end
+  end
 end
