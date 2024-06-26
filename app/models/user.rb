522b223de3a5d435b208d12a4b98408f12b7d98f
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-06-26 17:58:12 +0200

    create invoice after sign up method

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,20 +21,29 @@ class User < ApplicationRecord
   # initial invoice generation (after sign up)
 
   def create_initial_invoice
-    # product = Product.find_by(title: "Starter kit")
-    # return unless product
+    subscription = self.subscriptions.first
+    return unless subscription
 
-    # invoice = invoices.create(
-    #   issue_date: Time.current,
-    #   due_date: Time.current + 0.5.month,
-    # )
+    product_title = determine_product_title(subscription.duration, subscription.plan)
+    product = Product.find_by(title: product_title)
+    return unless product
 
-    # invoice.invoice_items.create(
-    #   product: product,
-    #   amount: product.price,
-    #   quantity: duration
-    # )
+    invoice = self.invoices.create(
+      issue_date: Time.current,
+      due_date: Time.current + 0.5.month,
+      total_amount: product.price
+    )
+
+    invoice.invoice_items.create(
+      product: product,
+      amount: product.price,
+      quantity: 1
+    )
   end
+
+  private
+
+
 
   ## phone number validation
 
@@ -64,6 +73,36 @@ class User < ApplicationRecord
     end
   end
 
+  # infer product title for first invoice based on subscription plan and duration
+
+  def determine_product_title(duration, plan)
+    case plan
+    when "standard"
+      case duration
+      when 1
+        "Standard 1 month subscription"
+      when 3
+        "Standard 3 month subscription"
+      when 6
+        "Standard 6 month subscription"
+      else
+        puts "Invalid duration"
+      end
+    when "XL"
+      case duration
+      when 1
+        "XL 1 month subscription"
+      when 3
+        "XL 3 month subscription"
+      when 6
+        "XL 6 month subscription"
+      else
+        puts "Invalid duration"
+      end
+    else
+      puts "Invalid plan"
+    end
+  end
 
 
 
