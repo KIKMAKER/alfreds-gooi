e56114d0afce1e69b5c1796e987bd3e155cb6607
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-07-02 16:29:04 +0200

    remove create drivers day job, combine into create collections day job

---
+++ app/jobs/create_collections_job.rb
@@ -0,0 +1,30 @@
+# app/jobs/create_drivers_day_job.rb
+class CreateCollectionsJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    # Only run on Tuesday, Wednesday, and Thursday
+    today = Date.today
+    if today.tuesday? || today.wednesday? || today.thursday?
+      # Create drivers_day entry
+      drivers_day = DriversDay.find_or_create_by!(
+        date: today,
+        user_id: User.find_by(role: 'driver').id
+      )
+      puts "Driver's Day processed for: #{drivers_day.user.first_name} with id: #{drivers_day.id}"
+    end
+
+    # Create collections for each subscription assigned to this day
+    subscriptions = Subscription.where(collection_day: today.wday)
+    subscriptions.each do |subscription|
+      collection = Collection.create!(
+        drivers_day: drivers_day,
+        subscription: subscription,
+        date: today,
+        # Add other necessary attributes
+      )
+    end
+    collection.update!(skip: true) if today >= subscription.holiday_start && today <= subscription.holiday_end
+
+  end
+end
--- test/jobs/create_collections_job_test.rb
+++ test/jobs/create_collections_job_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CreateDriversDayJobTest < ActiveJob::TestCase
+class CreateCollectionsJobTest < ActiveJob::TestCase
   # test "the truth" do
   #   assert true
   # end
