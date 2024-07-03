79da4faeb9f65345c651df3017b6ad2ca8b631e9
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-07-03 21:11:19 +0200

    rails admin and style improvements on some of alfies pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_navbar
+    if user_signed_in?
+      if current_user.driver? || current_user.admin?
+        render "shared/driver_navbar"
+      else
+        render "shared/new_navbar"
+      end
+    end
+    render "shared/new_navbar"
+  end
 end
