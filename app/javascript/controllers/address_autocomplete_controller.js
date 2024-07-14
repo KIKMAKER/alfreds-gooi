61a747519e910c73da75b6e85869c2e64d3ceaa9
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-07-14 10:10:25 +0200

    autofill working and styled

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -1,7 +1,6 @@
 // app/javascript/controllers/address_autocomplete_controller.js
 import { Controller } from "@hotwired/stimulus"
-import MapboxGeocoder from "https://unpkg.com/@mapbox/mapbox-gl-geocoder@5.0.0/dist/mapbox-gl-geocoder.min.js"
-
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="address-autocomplete"
 export default class extends Controller {
@@ -9,9 +8,23 @@ export default class extends Controller {
   static targets = ["address"]
 
   connect() {
+    // console.log("Mapbox Geocoder connected")
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
-      types: "country,region,place,postcode,locality,neighborhood,address"
+      types: "country,region,place,postcode,locality,neighborhood,address",
+      // Set proximity to a central point in Western Cape
+      proximity: {
+        longitude: 18.4239, // Longitude of Cape Town
+        latitude: -33.9189  // Latitude of Cape Town
+      },
+      // Set bbox to the bounding box of Western Cape
+      bbox: [
+        17.8333, // Minimum longitude
+        -34.8333, // Minimum latitude
+        21.2087, // Maximum longitude
+        -31.2930  // Maximum latitude
+      ],
+      countries: "ZA",
     })
     this.geocoder.addTo(this.element)
     this.geocoder.on("result", event => this.setInputValue(event))
