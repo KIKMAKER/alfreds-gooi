32ebe2fe370d7ec2058016745d937fd72c097bf4
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-10-12 10:37:17 +0200

    route optimiser combined

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
+
   patch 'optimise_route', to: 'collections#optimise_route'
   post "perform_create_collections", to: "collections#perform_create_collections"
   # Defines getting the csv - the form then sends the data to the import_csv route
