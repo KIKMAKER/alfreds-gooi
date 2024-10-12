8ce40799090ab72df647ef864c66a1b07b6b2626
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-10-12 12:37:30 +0200

    invoice create

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,11 +1,33 @@
 class InvoicesController < ApplicationController
 
   def new
-    @subscription = Subscription.find(params[:id])
+    @subscription = Subscription.find(params[:subscription_id])
     @invoice = Invoice.new
+    @products = Product.all
   end
+
+  def create
+    subscription = Subscription.find(params[:subscription_id])
+    invoice = Invoice.new(issued_date: Time.current, due_date: Time.current + 1.month)
+    invoice.subscription = subscription
+    invoice_item = InvoiceItem.new(invoice_item_params)
+    invoice_item.invoice = invoice
+    if invoice.save && invoice_item.save
+      redirect_to subscription_invoice_path(subscription, invoice)
+    else
+      render :new
+    end
+  end
+
   def show
     @subscription = Subscription.find(params[:subscription_id])
     @invoice = @subscription.invoices.first
+  end
+
+  private
+
+  def invoice_item_params
+    # params.require(:invoice).permit(:issued_date, :due_date, :subscription_id)
+    params.require(:invoice_items).permit(:product_id, :quantity)
   end
 end
