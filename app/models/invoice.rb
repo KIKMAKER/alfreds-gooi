76b3489fa70ebe82393ac1916f0c88a963523460
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-10-17 15:39:48 +0200

    invoice create with sub in form

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :subscription, dependent: :destroy
   has_one :user, through: :subscription
   has_many :invoice_items, dependent: :destroy
+  accepts_nested_attributes_for :invoice_items
 
   # validates :issued_date, :due_date, :total_amount, presence: true
   after_commit :set_number, on: :create
