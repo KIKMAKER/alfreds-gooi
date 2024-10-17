62b27004299cdce2bdb9e9c1085b816233b7a3d3
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-10-17 12:25:03 +0200

    invoice create is now nca

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,18 +4,16 @@ class InvoicesController < ApplicationController
     @subscription = Subscription.find(params[:subscription_id])
     @invoice = Invoice.new
     @products = Product.all
+    @invoice.invoice_items.build
   end
 
   def create
     subscription = Subscription.find(params[:subscription_id])
     @invoice = Invoice.new(issued_date: Time.current, due_date: Time.current + 1.month, total_amount: 0)
     @invoice.subscription = subscription
-    # invoice_item = InvoiceItem.new(invoice_item_params)
-    # invoice_item.amount = Product.find(invoice_item_params[:product_id]).price
 
-    # invoice_item.invoice = invoice
-    # invoice.calculate_total
     if @invoice.save
+
       create_invoice_items(@invoice)
       @invoice.calculate_total
       redirect_to subscription_invoice_path(subscription, @invoice), notice: 'Invoice was successfully created.'
@@ -34,20 +32,38 @@ class InvoicesController < ApplicationController
 
   def invoice_items_params
     # params.require(:invoice).permit(:issued_date, :due_date, :subscription_id)
-    params.require(:invoice_items).permit(product_id: [], quantity: [])
+    params.require(:invoice).permit(invoice_items_attributes: [ :product_id, :quantity ])
   end
 
   def create_invoice_items(invoice)
-    invoice_items_params[:product_id].each_with_index do |product_id, index|
-      product = Product.find(product_id)
-      quantity = invoice_items_params[:quantity][index].to_f
-      unless quantity == 0
-        invoice.invoice_items.create!(
-          product_id: product.id,
-          quantity: quantity,
-          amount: product.price * quantity
-        )
-      end
+    invoice_items_params[:invoice_items_attributes].each do |index, product_hash|
+      product = Product.find(product_hash[:product_id])
+      quantity = product_hash[:quantity]
+      next if quantity.blank? || quantity.to_f <= 0
+
+      invoice.invoice_items.create!(
+        product_id: product.id,
+        quantity: quantity,
+        amount: product.price * quantity.to_f
+      )
+
     end
+
+
+
+
+    # [:product_id]
+    # quantities = invoice_items_params[:invoice_items_attributes][:quantity]
+
+    # product_ids.each_with_index do |product_id, index|
+    # product = Product.find(product_id)
+    # quantity = quantities[index].to_f
+    #   unless quantity == 0
+    #     invoice.invoice_items.create!(
+    #       product_id: product.id,
+    #       quantity: quantity,
+    #       amount: product.price * quantity
+    #     )
+    #   end
   end
 end
