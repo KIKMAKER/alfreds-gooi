b0a4b4b4a3d391c4347405d92b17897efd838847
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-09 12:49:26 +0200

    fixing payment params

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -35,7 +35,7 @@ class PaymentsController < ApplicationController
       # Find the user by customer_id
       user = User.find_by(customer_id: payload["merchantReference"])
       # Find the invoice by the invoice_id
-      invoice = Invoice.find_by(id: payload["extra"]["invoiceId"].to_i)
+      invoice = Invoice.find_by(id: payload["extra"]["invoice_id"].to_i)
       if invoice.nil?
         Rails.logger.error "Invoice not found with id: #{payload['extra']['invoice_id']}"
         puts "Invoice not found with id: #{payload['extra']['invoice_id']}"
