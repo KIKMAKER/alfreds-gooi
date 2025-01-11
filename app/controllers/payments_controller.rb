e8c39c3a5c2f3445ee19320934d7d04312b5e780
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-11 21:04:24 +0200

    created job and triggered it in payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -72,7 +72,6 @@ class PaymentsController < ApplicationController
 
   private
 
-
   def handle_payment_payload(payment_data, user, invoice)
     Payment.create!(
       snapscan_id: payment_data["id"],
@@ -86,22 +85,21 @@ class PaymentsController < ApplicationController
       merchant_reference: payment_data["merchantReference"],
       user_id: user.id
     )
-
-    update_subscription_status(payment_data["merchantReference"])
+    subscription = Subscription.find_by(customer_id: payment_data["merchantReference"])
+    update_subscription_status(subscription)
+    CreateFirstCollectionsJob.perform_now(subscription)
     invoice.update(paid: true)
   end
 
-  def update_subscription_status(merchant_reference)
-    subscription = Subscription.find_by(customer_id: merchant_reference)
+  def update_subscription_status(subscription)
     if subscription
-      
       subscription.update!(
         status: 'active',
         start_date: subscription.calculate_next_collection_day
       )
-      puts "Subscription #{subscription.id} for customer #{merchant_reference} updated to active with start date #{subscription.start_date}."
+      puts "Subscription #{subscription.id} for customer #{subscription.customer_id} updated to active with start date #{subscription.start_date}."
     else
-      puts "No subscription found for customer #{merchant_reference}."
+      puts "No subscription found for customer #{subscription.customer_id}."
     end
   end
 
