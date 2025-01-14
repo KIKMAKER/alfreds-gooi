ae860ef65063befbf275012589e76a8d3f5289a2
Megan Street <meganjstreet@gmail.com>
2025-01-14 13:50:57 +0200

    Stuff I didnt commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,20 +1,23 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'donotreply@gooi.me'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            ENV['SMTP_USERNAME'],
-  password:            ENV['SMTP_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  Rails.application.configure do
+    config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_options = { from: 'howzit@gooi.me' }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'www.gooi.me', # Ensure this matches the sender's email domain
+      user_name:            ENV['SMTP_USERNAME'], # e.g., 'your-email@gmail.com'
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+  end
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
