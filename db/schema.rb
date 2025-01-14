79dc165ca0f3edac0558ec5ca6f5a205f39129c7
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-14 16:00:32 +0200

    referal code applied to user before create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_08_151102) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_14_135338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -155,6 +155,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_08_151102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "customer_id"
+    t.string "referral_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
