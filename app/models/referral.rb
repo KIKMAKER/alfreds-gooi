21d7b8a57ca3485d37743e856b382139c7df47b7
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-16 13:19:26 +0200

    referral instance created

---
+++ app/models/referral.rb
@@ -0,0 +1,8 @@
+class Referral < ApplicationRecord
+  belongs_to :referrer, class_name: 'User'
+  belongs_to :referee, class_name: 'User'
+  belongs_to :subscription, optional: true # Optional because not all referrals might be linked to a subscription immediately
+
+  validates :referrer_id, presence: true
+  validates :referee_id, presence: true
+end
