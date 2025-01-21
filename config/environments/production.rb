451c79bacd54186493f3ed03d8a1c3608d1cef64
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-21 16:05:57 +0200

    quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'gooi.me',
     authentication: :plain,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
