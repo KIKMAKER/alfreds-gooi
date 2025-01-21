aa5c786c5c7a55846d98ea499209219cb7767d71
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-01-21 16:05:57 +0200

    quick fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'devise/mailer'
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
@@ -27,7 +28,7 @@ Devise.setup do |config|
   config.mailer_sender = 'howzit@gooi.me'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'Devise::Mailer'
+  # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
@@ -225,7 +226,7 @@ Devise.setup do |config|
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
   config.reset_password_within = 6.hours
-  config.mailer.default_url_options = { host: 'alfred.gooi.me', protocol: 'https' }
+  # config.mailer.default_url_options = { host: 'alfred.gooi.me', protocol: 'https' }
 
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
