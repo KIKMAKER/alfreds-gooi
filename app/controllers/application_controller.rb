5a2a6e2ce56712926009767671afc93fd8debfce
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-02-03 16:33:33 +0200

    route to previous url after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :store_user_location!, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -8,5 +9,16 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :role])
+  end
+
+  private
+  # Store the original location before Devise intercepts
+  def store_user_location!
+    session[:user_return_to] = request.fullpath
+  end
+
+  # Define which requests should store the location
+  def storable_location?
+    request.get? && !devise_controller? && !request.xhr? # Only save for GET requests, not Devise/XHR requests
   end
 end
