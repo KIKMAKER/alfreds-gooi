7eebc503fbcd1fbba2b6f637af8186030f335eea
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-03-23 18:33:26 +0200

    tests all passing

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -1,6 +1,8 @@
 class DiscountCode < ApplicationRecord
 
   def available?
-    Date.today < expires_at
+    not_expired = expires_at.nil? || Date.today <= expires_at.to_date
+    under_limit = usage_limit.nil? || used_count < usage_limit
+    not_expired && under_limit
   end
 end
