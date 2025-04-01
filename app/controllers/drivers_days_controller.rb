75ea25c9775f4454d4db9ff7f52cdf2da09bc89e
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-04-01 14:17:06 +0200

    drivers day vamos page with whatsapp icons

--- app/controllers/drivers_days_controller.rb
+++ app/controllers/drivers_days_controller.rb
@@ -14,6 +14,34 @@ class DriversDaysController < ApplicationController
                                 end
   end
 
+
+  def vamos
+    # in production today will be the current day,
+    today = Date.today
+    # but in testing I want to be able to test the view for a given day
+    # today = Date.today  + 1
+    @today = today.strftime("%A")
+    @drivers_day = DriversDay.find_by(date: today)
+    if @drivers_day
+      @collections = @drivers_day.collections
+      @skip_collections = @collections.where(skip: true)
+      @new_customers = @collections.select { |collection| collection.new_customer == true }
+      @count = @collections.count - @skip_collections.count - (@new_customers.any? ? @new_customers.count : 0)
+      # @subscriptions = Subscription.active_subs_for(@today)
+      # @count_skip_subscriptions = Subscription.count_skip_subs_for(@today)
+      # @skip_subscriptions = @subscriptions.select { |subscription| subscription.collections.last&.skip == true }
+      @bags_needed = @collections.select { |collection| collection.needs_bags }
+
+      @hours_worked = @drivers_day.hours_worked unless @drivers_day.end_time.nil?
+    else
+      @collections = []
+      @new_customers = []
+      @count = 0
+      @bags_needed = []
+    end
+  end
+
+
   def start
     # in production today will be the current day,
     # today = "Wednesday"
@@ -69,7 +97,7 @@ class DriversDaysController < ApplicationController
     @total_buckets_collected = @collections&.sum(:buckets).floor || 0
     return unless request.patch?
 
-    if update_drivers_day(drivers_day_params, next_path: vamos_path)
+    if update_drivers_day(drivers_day_params, next_path: vamos_drivers_day_path)
       CreateCollectionsJob.perform_now
       CheckSubscriptionsForCompletionJob.perform_now
       puts "Driver's Day ended at: #{@drivers_day.end_time}"
