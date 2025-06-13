f5f3dde28a51c57c54c769688889edcf7ce99cc9
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-06-13 14:09:17 +0200

    sign out by delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,10 +38,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  # config.public_file_server.headers = {
-  #   "Access-Control-Allow-Origin"  => "https://www.gooi.me",
-  #   "Access-Control-Allow-Methods" => "GET, OPTIONS"
-  # }
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
