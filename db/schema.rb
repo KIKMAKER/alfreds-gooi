3dcd8a575e79e5cf964d7fd7b19e1bb7a6d49e2a
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-09-04 10:48:52 +0200

    only use discount code once

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_07_100600) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_04_084625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_07_100600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "legacy_subscription_id"
+    t.boolean "used_discount_code"
     t.index ["subscription_id"], name: "index_invoices_on_subscription_id"
   end
 
