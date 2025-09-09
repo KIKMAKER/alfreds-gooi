4e0f04eb64f8caa515fe2b2e48a6669b72b600e0
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-09-09 13:29:10 +0200

    admin users show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :discount_codes, only: [:index, :new, :create, :show]
-    resources :users, only: [:index, :edit, :update]
+    resources :users, only: [:index, :edit, :update, :show]
   end
 
   post 'snapscan/webhook', to: 'payments#snapscan_webhook'
