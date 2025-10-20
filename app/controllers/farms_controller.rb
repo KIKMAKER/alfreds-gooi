8b4f9cbd3365bfceef58cb552317457e8d85280f
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2025-10-20 15:38:54 +0200

    correcting farm slug params on all pages

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -6,7 +6,7 @@ class FarmsController < ApplicationController
   end
 
   def show
-    @farm = DropOffSite.find_by!(slug: params[:slug])
+    @farm = DropOffSite.find_by(slug: params[:slug]) || DropOffSite.find(params[:slug])
     @recent_events = @farm.drop_off_events.where(is_done: true).order(date: :desc).limit(10)
   end
 end
