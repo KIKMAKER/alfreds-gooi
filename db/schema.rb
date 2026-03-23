2bc49a5b1257a7842c3c588bef3832e4b35ffbed
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2026-03-23 13:25:14 +0200

    add missing trackable IP columns required by devise :trackable
    this is all claude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_18_100001) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_23_112028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -757,6 +757,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_18_100001) do
     t.string "provider"
     t.string "uid"
     t.string "referred_by_code"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
