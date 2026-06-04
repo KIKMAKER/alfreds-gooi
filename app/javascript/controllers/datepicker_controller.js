d5fb855b0412634ade8e686f92abc763764483f4
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2026-06-04 09:08:26 +0200

    Open datepicker calendar at today's month regardless of field value
    Previously the calendar opened at the pre-filled date (e.g. an old holiday
    from years ago). Now uses flatpickr's onOpen hook to jump the view to today
    while leaving the field value untouched.
    
    this one is all claude

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,9 @@ import flatpickr from "flatpickr";
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, { defaultDate: this.element.value || "today" })
+    flatpickr(this.element, {
+      defaultDate: this.element.value || "today",
+      onOpen: (_dates, _str, instance) => { instance.jumpToDate(new Date()) }
+    })
   }
 }
